9ae70a4f94cb9d21ca7fce93c690d76ea7823295
MisterProper9000 <danya_plaksin@mail.ru>
2018-12-16 18:06:32 +0300

    Add files via upload

---
+++ PseudoThreading/Grammar.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Даниил on 23.11.2018.
+ */
+public class Grammar {
+    public enum FILE_LEXEMES {INPUT_FILE, OUTPUT_FILE, PIPSEQ, WORKERS_FILE, }
+    public enum WORKERS_CFG_FILE_LEXEMES {WORKER, INPUT_WORKER, OUTPUT_WORKER}
+
+}
